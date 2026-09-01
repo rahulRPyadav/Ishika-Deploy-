965375af4ae36be136e19678488cf2a09098e3a6
ranandyadavAp <ranandyadav.tech@hotmail.com>
2026-09-01 09:30:12 +0530

    backedn url added

--- admin-portal/src/config/api.js
+++ admin-portal/src/config/api.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://localhost:5000';
+// export const API_BASE_URL = 'http://localhost:5000';
+export const API_BASE_URL = 'https://ishika-deploy.onrender.com';
 
 
 // Customer Website Frontend URL (Local vs Live)
