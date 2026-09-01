6f10b821fb43270b7368333f0ec07ec127ac05d4
Rahul  RP Yadav <ranandyadav.tech@hotmail.com>
2026-09-01 09:38:35 +0530

    Update api.js

--- admin-portal/src/config/api.js
+++ admin-portal/src/config/api.js
@@ -3,4 +3,4 @@ export const API_BASE_URL = 'https://ishika-deploy.onrender.com';
 
 
 // Customer Website Frontend URL (Local vs Live)
-export const FRONTEND_URL = 'http://localhost:5174';
+export const FRONTEND_URL = 'https://ishika-deploy-1.onrender.com/';
